245e085727d833989be7dbfdaae98537529bea73
Suchit <Suchit.choudhury@gmail.com>
2017-01-31 18:16:30 +0530

    initial

---
+++ Intro-MVC-6/src/Intro-MVC-6/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Intro_MVC_6.Data;
+using Intro_MVC_6.Models;
+
+namespace Intro_MVC_6.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private IApplicationRepository _repository;
+
+        public HomeController(IApplicationRepository repository)
+        {
+            _repository = repository;
+        }
+        public IActionResult Index()
+        {
+            return View(_repository.GetAllToDos());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Intro-MVC-6/src/Intro-MVC-6/Models/ApplicationRepository.cs
@@ -0,0 +1,28 @@
+﻿using Intro_MVC_6.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intro_MVC_6.Models
+{
+    public class ApplicationRepository:IApplicationRepository
+    {
+
+        private ApplicationDbContext _dbcontext;
+
+        public ApplicationRepository(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public List<ToDo> GetAllToDos()
+        {
+            return _dbcontext.ToDos.ToList();
+        }
+    }
+
+    public interface IApplicationRepository
+    {
+         List<ToDo> GetAllToDos();
+    }
+}
